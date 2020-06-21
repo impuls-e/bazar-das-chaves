2467b282e977e3797442d6344d3ed72101d88e61
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-21 09:56:07 -0300

    change the width of sitedescription

--- data/config.js
+++ data/config.js
@@ -1,9 +1,9 @@
 const config = {
   siteTitle: "Bazar das Chaves e Carimbos - Serviços com qualidade e confiança",
   descriptionTemplate:
-    "Um chaveiro em Florianópolis com mais de 20 anos de mercado. Fazemos cópias de chaves, carimbos, controles, tags, troca fechaduras, entre outros... ",
+    "Um chaveiro em Florianópolis com mais de 20 anos de mercado. Fazemos cópias de chaves, carimbos, controles, tags, troca fechaduras, entre outros..",
   siteDescription:
-    "Um chaveiro com mais de 20 anos de mercado. Serviços com qualidade e confiança. Fazemos cópias de chaves, carimbos, controles, abertura de cofres, fechaduras, troca de segredos, entre outros...",
+    "Um chaveiro em Florianópolis com mais de 20 anos de mercado. Fazemos cópias de chaves, carimbos, controles, tags, troca fechaduras, entre outros..",
   siteTitleShort: "BDCC",
   siteRss: "/rss.xml",
   siteImage: `${__dirname}/src/img/circle-logo.jpeg`,
