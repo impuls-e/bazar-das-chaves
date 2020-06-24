722228c1ed366b7e906d3fbf17a31d77507c54dc
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-23 21:04:31 -0300

    remove animation of Hero component

--- src/containers/Hero/index.js
+++ src/containers/Hero/index.js
@@ -33,23 +33,9 @@ export default function Hero() {
 
       <Container>
         <HeroContent>
-          <h1
-            data-sal="slide-up"
-            data-sal-delay="200"
-            data-sal-easing="ease"
-            data-sal-duration="1000"
-          >
-            Bazar das Chaves e Carimbos
-          </h1>
+          <h1>Bazar das Chaves e Carimbos</h1>
           <div>
-            <h2
-              data-sal="slide-up"
-              data-sal-delay="200"
-              data-sal-easing="ease"
-              data-sal-duration="1000"
-            >
-              Confiança e qualidade no centro de Florianópolis
-            </h2>
+            <h2>Confiança e qualidade no centro de Florianópolis</h2>
             <Img
               fluid={data.desktop.childImageSharp.fluid}
               alt="An image apresentation from current project"
